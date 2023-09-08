18fdc159ef82cab3560aad1ad731c44b8769cfe2
Anand Mohan Pathak <anandpathak8317@gmail.com>
2023-09-08 13:22:10 +0530

    Test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe 'Class Solver Check' do
-	
+	let(:solver) { Solver.new }
+
+	it 'Factorial Method Check' do
+		expect(solver.factorial(0)).to eq(1)
+	    expect(solver.factorial(1)).to eq(1)
+	    expect(solver.factorial(5)).to eq(120)
+	    expect {solver.factorial(-5) }.to raise_error(ArgumentError, "Factorial is not defined for negative numbers")
+
+	end
 end
